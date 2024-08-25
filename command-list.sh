54b2f3ee1a67de4de9d81db71731c7bd01e02f0b
ajitaro <ftaqiy@gmail.com>
2024-08-25 21:52:49 +0700

    feat: setup postgres

--- command-list.sh
+++ command-list.sh
@@ -6,3 +6,12 @@ docker compose run --rm app sh -c "django-admin startproject app ."
 
 # Start development server
 docker compose up
+
+# Test
+docker compose run --rm app sh -c "python manage.py test"
+
+# Build, build everytime Dockerfile changes
+docker compose build
+
+# Clean
+docker compose down
