429a191104f39fd361f99ca16bf820d6c24eadb7
ajitaro <ftaqiy@gmail.com>
2024-08-26 21:41:23 +0700

    feat: custom user model

--- command-list.sh
+++ command-list.sh
@@ -9,6 +9,8 @@ docker compose up
 
 # Test
 docker compose run --rm app sh -c "python manage.py test"
+
+# Linting
 docker compose run --rm app sh -c "python manage.py wait_for_db"
 docker compose run --rm app sh -c "python manage.py wait_for_db && flake8"
 
@@ -20,3 +22,12 @@ docker compose down
 
 # Create new django app
 docker compose run --rm app sh -c "python manage.py startapp core"
+
+# Make migrations
+docker compose run --rm app sh -c "python manage.py makemigrations"
+
+# Migrate
+docker compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"
+
+# clear database in volume
+docker volume rm recipe-app-api_dev-db-data
