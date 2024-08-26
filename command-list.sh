b50c26be6dc0ebd40ed0d82713e629c7f14a9a32
ajitaro <ftaqiy@gmail.com>
2024-08-26 21:01:17 +0700

    feat: configure fdatabase

--- command-list.sh
+++ command-list.sh
@@ -9,9 +9,14 @@ docker compose up
 
 # Test
 docker compose run --rm app sh -c "python manage.py test"
+docker compose run --rm app sh -c "python manage.py wait_for_db"
+docker compose run --rm app sh -c "python manage.py wait_for_db && flake8"
 
 # Build, build everytime Dockerfile changes
 docker compose build
 
 # Clean
 docker compose down
+
+# Create new django app
+docker compose run --rm app sh -c "python manage.py startapp core"
